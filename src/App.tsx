f6c2ae39a95d1105ec6d2a738fcb1b09254e629f
Jamshid <jamshidabduraxmonov164@gmail.com>
2026-07-10 11:02:38 +0400

    feat: initial UI of the 'Thoughts' page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import DisplayThoughts from './thoughts.tsx'
+
 
 function App() {
   
 
   return (
     <> 
-    <h1 className="">Hello!</h1>
+      <DisplayThoughts />
     </>
   )
 }
---
+++ src/thoughts.tsx
@@ -0,0 +1,33 @@
+import mockData from './assets/mock.json'
+
+type Thought = {
+    id: number;
+    content: string;
+    timestamp: string
+};
+
+
+export function OneThought({thought} : {thought : Thought}) {
+    return(
+        <div className="border">
+            <p>{thought.content}</p>
+            <small>{thought.timestamp}</small>
+        </div>
+    )
+}
+
+
+
+
+export default function DisplayThoughts(){
+
+    return(
+        <div className="flex flex-col gap-4 p-4 w-[50%] m-auto">
+        {
+            mockData.map((thought : Thought)=> (
+                <OneThought thought={thought}/>
+            ))
+        }
+        </div>
+    )
+}
