1e79374e494c59b70cb49cac987ac8b28df0aec2
Jamshid <jamshidabduraxmonov164@gmail.com>
2026-07-11 09:38:04 +0400

    feat: implement UI for content posting and dark Theme

--- src/thoughts.tsx
+++ src/thoughts.tsx
@@ -9,9 +9,9 @@ type Thought = {
 
 export function OneThought({thought} : {thought : Thought}) {
     return(
-        <div className="border rounded p-2">
-            <p className="font-roboto">{thought.content}</p>
-            <small>{thought.timestamp}</small>
+        <div className="border border-white rounded p-2">
+            <p className="font-roboto text-white">{thought.content}</p>
+            <small className="text-white">{thought.timestamp}</small>
         </div>
     )
 }
@@ -22,12 +22,20 @@ export function OneThought({thought} : {thought : Thought}) {
 export default function DisplayThoughts(){
 
     return(
-        <div className="flex flex-col gap-4 p-4 w-[50%] m-auto">
-        {
-            mockData.map((thought : Thought)=> (
-                <OneThought thought={thought}/>
-            ))
-        }
+        <div className="bg-zinc-950 text-white">
+            <div className="flex flex-col gap-4 p-4 w-[50%] m-auto">
+            {
+                mockData.map((thought : Thought)=> (
+                    <OneThought thought={thought}/>
+                ))
+            }
+            </div>
+
+            <div className="w-[100%] bg-zinc-950 sticky bottom-0 flex  rounded m-auto p-4">
+                <textarea className="text-white bg-zinc-800 border rounded m-auto w-[80%] p-2"/>
+                <button className="border px-4 rounded">Post</button>
+            </div>
         </div>
+        
     )
 }
